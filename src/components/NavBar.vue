59cc9c492c2c4fac11324d595de4ffb42c813cb1
YBK1 <kyb031700@naver.com>
2024-05-16 21:37:04 +0900

    fix: 세션스토리지 변수명 통일

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,15 @@
 <template>
-    <div class="container sticky-top zindex-sticky top-0">
+  <div class="container sticky-top zindex-sticky top-0">
     <div class="row">
       <div class="col-12">
         <nav
           class="blur navbar navbar-expand-lg top-0 zindex-fixed shadow position-absolute my-3 py-2 start-0 end-0 w-100"
           style="background-color: #f5efe6"
         >
-          <a class="navbar-brand ms-4 pt-3 pb-2" href="/" style="color: #4b555e; font-size: 2em; font-weight: 800;"
+          <a
+            class="navbar-brand ms-4 pt-3 pb-2"
+            href="/"
+            style="color: #4b555e; font-size: 2em; font-weight: 800"
             >#Enjoy_Trip</a
           >
           <button
@@ -21,20 +24,32 @@
             <!-- <b-icon-list></b-icon-list> -->
           </button>
 
-          <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+          <div
+            class="collapse navbar-collapse justify-content-center"
+            id="navbarSupportedContent"
+          >
             <ul class="navbar-nav ms-auto me-auto">
               <li class="nav-item" @click="changed">
-                <router-link id="apt" to="/trip" class="ml-3 mr-3 nav-link text-secondary"
+                <router-link
+                  id="apt"
+                  to="/trip"
+                  class="ml-3 mr-3 nav-link text-secondary"
                   >관광지검색</router-link
                 >
               </li>
               <li class="nav-item" @click="changed">
-                <router-link id="notice" to="/notice" class="ml-3 mr-3 nav-link text-secondary"
+                <router-link
+                  id="notice"
+                  to="/notice"
+                  class="ml-3 mr-3 nav-link text-secondary"
                   >공지사항</router-link
                 >
               </li>
               <li class="nav-item" @click="changed">
-                <router-link id="qna" to="/qna" class="ml-3 mr-3 nav-link text-secondary"
+                <router-link
+                  id="qna"
+                  to="/qna"
+                  class="ml-3 mr-3 nav-link text-secondary"
                   >QnA</router-link
                 >
               </li>
@@ -50,21 +65,56 @@
                   aria-haspopup="true"
                   aria-expanded="false"
                 >
-                <i v-if="!userInfo" class="bi bi-people" style="font-size: 2rem; padding-right: 10px;"></i>
-                <i v-else class="bi bi-people-fill" style="font-size: 2rem; padding-right: 10px;"></i>
+                  <i
+                    v-if="!userInfo"
+                    class="bi bi-people"
+                    style="font-size: 2rem; padding-right: 10px"
+                  ></i>
+                  <i
+                    v-else
+                    class="bi bi-people-fill"
+                    style="font-size: 2rem; padding-right: 10px"
+                  ></i>
                 </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="background-color: white">
-                  <router-link id="userpage" to="/userpage" class="dropdown-item" v-if="userInfo">
+                <div
+                  class="dropdown-menu dropdown-menu-right"
+                  aria-labelledby="navbarDropdown"
+                  style="background-color: white"
+                >
+                  <router-link
+                    id="userpage"
+                    to="/userpage"
+                    class="dropdown-item"
+                    v-if="userInfo"
+                  >
                     마이페이지
                   </router-link>
-                  <router-link id="join" to="/signup" class="dropdown-item" v-if="!userInfo">
+                  <router-link
+                    id="join"
+                    to="/signup"
+                    class="dropdown-item"
+                    v-if="!userInfo"
+                  >
                     회원가입
                   </router-link>
-                  <div class="dropdown-divider" style="border: #7895b2 1px"></div>
-                  <a href="" v-if="userInfo" class="dropdown-item" @click="userLogout()">
+                  <div
+                    class="dropdown-divider"
+                    style="border: #7895b2 1px"
+                  ></div>
+                  <a
+                    href=""
+                    v-if="userInfo"
+                    class="dropdown-item"
+                    @click="userLogout()"
+                  >
                     로그아웃
                   </a>
-                  <router-link v-else id="login" to="/login" class="dropdown-item">
+                  <router-link
+                    v-else
+                    id="login"
+                    to="/login"
+                    class="dropdown-item"
+                  >
                     로그인
                   </router-link>
                 </div>
@@ -78,17 +128,15 @@
 </template>
 
 <script setup>
-const userInfo = sessionStorage.getItem("userInfo");
+const userInfo = sessionStorage.getItem("jwt");
 
 const userLogout = () => {
   if (!userInfo) {
     alert("오류가 발생했습니다.");
+  } else {
+    sessionStorage.removeItem("jwt");
   }
-  else {
-    sessionStorage.removeItem('userInfo');
-  }
-}
-
+};
 </script>
 
 <style scoped>
