9fddb2e98e76a12b9e89083aaaa84feac29b63ae
김영빈 <kyb031700@naver.com>
2024-05-21 10:48:21 +0900

    feat: 메인화면 뉴스 크롤링 추가

--- src/components/home/HomeDetail.vue
+++ src/components/home/HomeDetail.vue
@@ -3,13 +3,6 @@
   <div class="HomeMain">
     <p style="font-size: 4rem; font-family: LINESeedKR-Bd">AI를 통해 추천받으세요</p>
     <p style="font-size: 4rem; font-family: LINESeedKR-Bd">Travel Pick</p>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <img src="@/assets/down_arrow.png" width="40px" height="40px" style="margin: 0 auto; " />
   </div>
 
   <div class="HomeMid1">
@@ -37,20 +30,41 @@
     <p style="font-size: 2rem; font-family: LINESeedKR-Bd">
       나의 성향에 따른 맞춤형 여행지를 추천해드리겠습니다.
     </p>
+    <br>
+    <br>
+    <br>
 
+    <div class="row">
+      <div class = "col-1"></div>
+      <div class="card col-4 HomeCard">
+        <a href='/trip'><img src="@/assets/maincard1.png" class="card-img-top mt-5" alt="..." style='height: 300px'></a>
+        <div class="card-body">
+          <p class="card-text">위치를 기반으로 관광지를 검색하고 그와 유사한 관광지에 대해서도 알아보세요</p>
+        </div>
+      </div>
+      <div class = "col-2">
+
+      </div>
+      <div class="card col-4 HomeCard">
+        <img src= "@/assets/maincard2.png" class="card-img-top mt-5" alt="..." style='height: 300px'>
+        <div class="card-body">
+          <p class="card-text">방문했던 여행지가 정말 좋았다면 내 관광지에 저장해보세요. 내 관광지 목록을 통해서 비슷한 여행지를 추천해드릴게요</p>
+        </div>
+      </div>
+    </div>
+
+    <NewsView></NewsView>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import NewsView from './NewsView.vue';
 </script>
 <style scoped src="../../views/style/style.css"></style>
 <style scoped>
 .HomeMain {
   width: 100%;
-  height: 840px;
+  height: 420px;
   /* background-color: rgb(187, 149, 149); */
   text-align: center;
   padding: 10rem;
@@ -64,7 +78,7 @@ export default {
 }
 .HomeMid1Left {
   width: 50%;
-  height: 100%;
+  height: 500px;
   /* background-color: rgb(84, 61, 61); */
   float: left;
   padding-left: 8rem;
@@ -80,9 +94,12 @@ export default {
 }
 .HomeMid2 {
   width: 100%;
-  height: 500px;
+  height: 900px;
   background-color: #D8BFD8;
   text-align: center;
   padding-top: 10rem;
+}
+.HomeCard{
+  background-color: #e6e6fa;
 }
 </style>
---
+++ src/components/home/NewsView.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { Carousel, Slide } from 'vue3-carousel';
+import 'vue3-carousel/dist/carousel.css';
+
+import http from "@/api/http-common";
+
+import NewsDetailItem from './NewsDetailItem.vue';
+
+const newsList = ref([]);
+
+onMounted(() => {
+    http.get(`/api/news/list/여행지`)
+        .then((response) => {
+            console.log(response.data.data);
+          newsList.value = response.data.data;
+        })
+        .catch((error) => {
+          alert(error.message);
+        });
+})
+
+</script>
+
+<template>
+    <div class="row" style='height: 600px; margin-top: 150px'>
+        <h3>어디를 가야할지 너무 막막하다면? 뉴스로부터 시작해보는 것도 좋아요!</h3>
+        <div class="carousel-container">
+            <Carousel :itemsToShow="3">
+            <template v-slot:default="{ prev, next }">
+                <Slide v-for="(news, index) in newsList" :key="index">
+                    <NewsDetailItem :news="news"> </NewsDetailItem>
+                </Slide>
+                <button @click="prev" class="carousel-button prev-button">‹</button>
+                <button @click="next" class="carousel-button next-button">›</button>
+            </template>
+            </Carousel>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.carousel-container {
+  max-width: 1700px; /* 캐러셀의 최대 너비를 설정 */
+  margin: auto; /* 가운데 정렬 */
+  position: relative; /* 버튼 위치를 위해 추가 */
+}
+
+.carousel-button {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  z-index: 10;
+}
+
+.prev-button {
+  left: 10px;
+}
+
+.next-button {
+  right: 10px;
+}
+</style>
