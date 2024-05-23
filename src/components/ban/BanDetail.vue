f37387b8bf6d6e80f6c7666d04baa7032b8d946a
thswltjr11 <thswltjr11@gmail.com>
2024-05-23 17:02:19 +0900

    feat: 찐막

--- src/components/ban/BanDetail.vue
+++ src/components/ban/BanDetail.vue
@@ -6,24 +6,34 @@
     <div class="card mb-3 m-auto">
       <div class="card-header text-right" style="font-size: 0.8em">
         등록 날짜 : {{ ban.createDate }}<br />
-        <div>
-          <span v-if='ban.toxicity > 0.6' class="badge bg-warning text-light">유해성</span>
-          <span v-if='ban.severeToxicity > 0.6' class="badge bg-danger text-light">심한 유해성</span>
-          <span v-if='ban.identityAttack > 0.6' class="badge text-light" style='background-color: purple'>인신공격</span>
-          <span v-if='ban.insult > 0.6' class="badge text-light" style='background-color: green'>모욕</span>
-          <span v-if='ban.profanity > 0.6' class="badge text-light" style='background-color: darkblue'>비속어</span>
-          <span v-if='ban.threat > 0.6' class="badge bg-dark text-light">폭력성</span>
-        </div>
       </div>
       <div class="card-body" style="height: 300px">
         <p class="card-text text-left" v-html="ban.content"></p>
       </div>
+      <div class="fix-mark">
+        <div v-if='ban.toxicity > 0.6' class="badge bg-warning text-light ban-mark">
+          유해성 : {{ (ban.toxicity * 100).toFixed(2) }} %
+        </div>
+        <div v-if='ban.severeToxicity > 0.6' class="badge bg-danger text-light ban-mark">
+          심한 유해성 : {{ (ban.severeToxicity * 100).toFixed(2) }} %
+        </div>
+        <div v-if='ban.identityAttack > 0.6' class="badge text-light ban-mark" style='background-color: purple'>
+          인신공격 : {{ (ban.identityAttack * 100).toFixed(2) }} %
+        </div>
+        <div v-if='ban.insult > 0.6' class="badge text-light ban-mark" style='background-color: green'>
+          모욕 : {{ (ban.insult * 100).toFixed(2) }} %
+        </div>
+        <div v-if='ban.profanity > 0.6' class="badge text-light ban-mark" style='background-color: darkblue'>
+          비속어 : {{ (ban.profanity * 100).toFixed(2) }} %
+        </div>
+        <div v-if='ban.threat > 0.6' class="badge bg-dark text-light ban-mark">
+          폭력성 : {{ (ban.threat * 100).toFixed(2) }} %
+        </div>
+      </div>
     </div>
     <br />
     <div class="d-flex justify-content-end">
-      <router-link class="btn btn-dark m-3" to="/userpage/banned"
-        >목록으로</router-link
-      >
+      <router-link class="btn btn-dark m-3" to="/userpage/banned">목록으로</router-link>
     </div>
     <br />
   </div>
@@ -56,8 +66,20 @@ onMounted(() => {
 h4 {
   font-weight: 600;
 }
+
 .card {
   border: #ffffff solid 1px;
   border-bottom-color: rgba(0, 0, 0, 0.125);
+}
+
+.ban-mark {
+  font-size: 18px;
+  margin-bottom: 10px;
+  display: block !important;
+}
+.fix-mark{
+  position: absolute;
+  right: 20px;
+  top: 130px;
 }
 </style>
