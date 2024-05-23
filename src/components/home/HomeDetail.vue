4f557a88a168e1a7b3a35b17dae8eb01c7ce2a9b
thswltjr11 <thswltjr11@gmail.com>
2024-05-23 15:14:56 +0900

    feat: MainView 수정

--- src/components/home/HomeDetail.vue
+++ src/components/home/HomeDetail.vue
@@ -21,20 +21,16 @@
   </div>
 
   <div class="HomeMid2">
-    <p style="font-size: 2rem; font-family: LINESeedKR-Bd">
+    <p style="font-size: 2.5rem; font-family: LINESeedKR-Bd">
       내가 좋아할 관광지만 Pick
     </p>
-    <p style="font-size: 2rem; font-family: LINESeedKR-Bd">
+    <p style="font-size: 2.5rem; font-family: LINESeedKR-Bd">
       Travel Pick만의 추천 알고리즘으로 관광지를 쉽게 선택하세요!
     </p>
-    <p style="font-size: 2rem; font-family: LINESeedKR-Bd">
+    <p style="font-size: 2.5rem; font-family: LINESeedKR-Bd">
       나의 성향에 따른 맞춤형 여행지를 추천해드리겠습니다.
     </p>
-    <br>
-    <br>
-    <br>
-
-    <div class="row">
+    <!-- <div class="row">
       <div class = "col-1"></div>
       <div class="card col-4 HomeCard">
         <img src="@/assets/maincard1.png" class="card-img-top mt-5" alt="..." style='height: 300px' @click="$router.push('/trip')">
@@ -53,12 +49,78 @@
       </div>
     </div>
 
-    <NewsView></NewsView>
+    <NewsView></NewsView> -->
   </div>
+
+  <div class="HomeMid3">
+    <div class="HomeMid3Left">
+      <p style="font-size: 4rem; font-family: LINESeedKR-Bd">관광지 정보 제공</p>
+      <p style="font-size: 1.5rem; font-family: LINESeedKR-Bd; color: gray">
+        위치를 확인하고 정보를 얻어보세요!
+      </p>
+      <p style="font-size: 1.5rem; font-family: LINESeedKR-Bd; color: gray">
+        관광지를 선택하면 유사한 관광지도 추천해요
+      </p>
+    </div>
+    <div class="HomeMid3Right">
+      <img src="@/assets/home3.gif" width="700px" style="
+            margin: 0 auto;
+            margin-top: 10%;
+            border-radius: 2rem;
+            filter: drop-shadow(5px 5px 5px #000);
+          " />
+    </div>
+  </div>
+
+  <div class="HomeMid4">
+    <div class="HomeMid3_1Left">
+      <img src="@/assets/home4.gif" width="700px" style="
+            margin: 0 auto;
+            margin-top: 0%;
+            margin-left: 30%;
+            border-radius: 2rem;
+            filter: drop-shadow(5px 5px 5px #000);
+          " />
+    </div>
+    <div class="HomeMid3_1Right">
+      <p style="font-size: 4rem; font-family: LINESeedKR-Bd">추천 서비스 제공</p>
+      <p style="font-size: 1.5rem; font-family: LINESeedKR-Bd; color: gray">
+        나의 관심목록으로 관광지를 추천받을 수 있어요!
+      </p>
+      <p style="font-size: 1.5rem; font-family: LINESeedKR-Bd; color: gray">
+        내가 좋아할 관광지만 쉽게 선택하세요
+      </p>
+    </div>
+  </div>
+
+  <div class="HomeMid5">
+    <div class="HomeMid3Left">
+      <p style="font-size: 4rem; font-family: LINESeedKR-Bd">최단 경로 제공</p>
+      <p style="font-size: 1.5rem; font-family: LINESeedKR-Bd; color: gray">
+        관심 목록에 관광지만 넣어주세요
+      </p>
+      <p style="font-size: 1.5rem; font-family: LINESeedKR-Bd; color: gray">
+        Travel Pick이 거리를 다 계산해드릴게요!
+      </p>
+    </div>
+    <div class="HomeMid3Right">
+      <img src="@/assets/home5.gif" width="700px" style="
+            margin: 0 auto;
+            margin-top: 10%;
+            border-radius: 2rem;
+            filter: drop-shadow(5px 5px 5px #000);
+          " />
+    </div>
+  </div>
+
+  <div class="HomeFoot">
+      <p style="font-size: 4rem; font-family: LINESeedKR-Bd">이제 Travel Pick 만의 서비스를 즐겨보세요!</p>
+      <p style="font-size: 4rem; font-family: LINESeedKR-Bd"></p>
+    </div>
 </template>
 
 <script setup>
-import NewsView from './NewsView.vue';
+// import NewsView from './NewsView.vue';
 </script>
 <style scoped src="../../views/style/style.css"></style>
 <style scoped>
@@ -69,6 +131,7 @@ import NewsView from './NewsView.vue';
   text-align: center;
   padding: 10rem;
 }
+
 .HomeMid1 {
   width: 100%;
   height: 500px;
@@ -76,6 +139,7 @@ import NewsView from './NewsView.vue';
   padding-right: 15rem;
   /* background-color: rgb(112, 92, 92); */
 }
+
 .HomeMid1Left {
   width: 50%;
   height: 500px;
@@ -83,6 +147,7 @@ import NewsView from './NewsView.vue';
   float: left;
   padding-left: 8rem;
 }
+
 .HomeMid1Right {
   width: 50%;
   height: 100%;
@@ -92,14 +157,102 @@ import NewsView from './NewsView.vue';
   padding-left: 6rem;
   padding-top: 8rem;
 }
+
 .HomeMid2 {
   width: 100%;
-  height: 900px;
+  height: 500px;
   background-color: #D8BFD8;
   text-align: center;
   padding-top: 10rem;
 }
-.HomeCard{
+
+.HomeCard {
   background-color: #e6e6fa;
+}
+
+.HomeMid3Left {
+  width: 50%;
+  height: 100%;
+  /* background-color: rgb(84, 61, 61); */
+  float: left;
+  padding-top: 9rem;
+  padding-left: 20rem;
+}
+
+.HomeMid3Right {
+  width: 50%;
+  height: 100%;
+  /* background-color: rgb(215, 208, 208); */
+  float: left;
+}
+
+.HomeMid3 {
+  width: 100%;
+  height: 500px;
+  /* background-color: rgb(117, 57, 57); */
+}
+
+.HomeMid3_1 {
+  width: 100%;
+  height: 500px;
+  /* background-color: rgb(238, 232, 232); */
+}
+
+.HomeMid3_1Left {
+  width: 50%;
+  height: 100%;
+  /* background-color: rgb(214, 214, 214); */
+  float: left;
+}
+
+.HomeMid3_1Right {
+  width: 50%;
+  height: 100%;
+  /* background-color: rgb(204, 140, 140); */
+  float: left;
+}
+
+.HomeMid3_1RightImg {
+  background-color: aliceblue;
+  width: 50%;
+  height: 100%;
+  float: left;
+}
+
+.HomeMid3_1RightInfo {
+  float: left;
+}
+
+.HomeMid4 {
+  margin-top: 100px;
+  width: 100%;
+  height: 400px;
+  /* background-color: rgb(187, 149, 149); */
+}
+
+.HomeMid4_1 {
+  width: 100%;
+  height: 500px;
+  background-color: rgb(238, 232, 232);
+}
+
+.HomeMid4_2 {
+  width: 100%;
+  height: 500px;
+  background-color: rgb(238, 232, 232);
+}
+
+.HomeMid5 {
+  width: 100%;
+  height: 600px;
+  /* background-color: rgb(187, 149, 149); */
+}
+
+.HomeFoot {
+  width: 100%;
+  height: 400px;
+  padding-left: 10rem;
+  padding-top: 10rem;
+  background-color: rgb(255, 230, 227);
 }
 </style>
